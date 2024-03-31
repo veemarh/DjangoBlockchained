9e22ec9a891af2b95c77f685d1d2fbad0d51461d
Denis Pelipeychenko <pelipejchenko.da@edu.spbstu.ru>
2024-03-31 21:00:40 +0300

    work with homepage and users

--- djangoBlockchained/settings.py
+++ djangoBlockchained/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
