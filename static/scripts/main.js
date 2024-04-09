6bb88c483df8e178e552568b26f0db7a902a5c1a
veta <vee.marh.my@gmail.com>
2024-04-09 18:19:24 +0300

    upgraded navigation menu

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,17 +1,45 @@
-/* появляет выпадающее меню пользователя в шапке */
-function OpenUserMenu(id) {
-    let elem = document.getElementById(id);
-    (elem.style.display !== 'block') ? (elem.style.display = 'block') : (elem.style.display = '');
-}
-/* скрывает меню пользователя, если тыкаешь где-то на странице */
-window.onclick = function(event) {
-    if (!event.target.matches('.header-dropdown-button span')) {
-        let dropdownApps = document.getElementsByClassName('header-dropdown-content');
-        for (let i = 0; i < dropdownApps.length; i++) {
-            let openDropdown = dropdownApps[i];
-            if (openDropdown.style.display === 'block' ) {
-                openDropdown.style.display = 'none';
-            }
+/* на будущее -- нужно будет создать два разных js */
+/* для авторизованных пользователей отдельный файл, для анонимных отдельный */
+
+document.addEventListener('DOMContentLoaded', function() {
+    /* события для навигационного меню в мобильном режиме */
+    let menuFixed = false;
+    let menuToggle = document.getElementById('nav-toggle');
+    let menu = document.getElementById('nav-left');
+    menuToggle.addEventListener('mouseover', function() {
+        menu.classList.add('active');
+    });
+    menu.addEventListener('mouseleave', function() {
+        if (!menuFixed) menu.classList.remove('active');
+    });
+    menuToggle.addEventListener('click', function() {
+        if (menuFixed) {
+            menu.classList.remove('active');
         }
-    }
-}
+        else {
+            menu.classList.add('active');
+        }
+        menuFixed = !menuFixed;
+        menuToggle.firstElementChild.style.hidden=true;
+    });
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.classList.remove('active');
+            menuFixed = false;
+        }
+    });
+
+    /* события для пользователя */
+    /* появляет окно пользователя по клику на иконку */
+    let userIcon = document.getElementById('user-icon');
+    let userApp = document.getElementById('header-user-menu');
+    userIcon.addEventListener('click', function() {
+        userApp.classList.toggle('active');
+    });
+    /* скрывает окно пользователя, если тыкаешь где-то на странице */
+    document.addEventListener('click', function(event) {
+        if (!userApp.contains(event.target) && event.target !== userIcon) {
+            userApp.classList.remove('active');
+        }
+    });
+});
