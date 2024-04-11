10f9a3e01aa210585c67268e262a77240aa991e0
Denis Pelipeychenko <pelipejchenko.da@edu.spbstu.ru>
2024-04-11 23:00:49 +0300

    forms for student little fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,20 +3,19 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import StudentUser, TeacherUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class StudentUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = StudentUser
         fields = (
             "first_name",
             "second_name",
             "third_name",
-            "phone_number"
-            "email",
+            "phone_number" "email",
             "age",
         )
 
 
-class CustomUserChangeForm(UserChangeForm):
+class StudentUserChangeForm(UserChangeForm):
     class Meta:
         model = StudentUser
         fields = (
@@ -25,7 +24,7 @@ class CustomUserChangeForm(UserChangeForm):
             # "third_name",
             "phone_number",
             "school_name",
-            "birth_date"
-            "email",
+            "birth_date" "email",
             "age",
         )
+
