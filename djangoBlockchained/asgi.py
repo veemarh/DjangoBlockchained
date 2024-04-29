422f6acc7a1ef41669a5f96357fa10e8f7f1ad9c
Max <lonishin.max@gmail.com>
2024-04-29 19:13:01 +0300

    add a command for chat application

--- djangoBlockchained/asgi.py
+++ djangoBlockchained/asgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoBlockchained.settings")
 
+from chat import routing
+
 application = get_asgi_application()
+
+chat_application = ProtocolTypeRouter(
+    {
+        'http': application,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        )
+    }
+)
