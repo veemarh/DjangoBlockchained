3e809cba96606a2581aaac7536080dbf24595e1a
Denis Pelipeychenko <pelipejchenko.da@edu.spbstu.ru>
2024-05-07 10:55:00 +0300

    forms updated

--- accounts/views.py
+++ accounts/views.py
@@ -65,7 +65,7 @@ class TeacherSignUpView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect("")
+        return redirect("home")
 
 
 class StudentProfileView(LoginRequiredMixin, DetailView):
--- teacher/views.py
+++ teacher/views.py
@@ -7,4 +7,3 @@ class TeacherListView(ListView):
     model = Teacher
     context_object_name = "teachers"  # Optional: custom name for context
     template_name = "teacher_list.html"
-    queryset = Teacher.objects.all()
